d3a59a6562b343818f0e66d99cbe0beb0ba3f710
Long Roos <long@genesisaugmented.com>
2019-11-16 17:40:09 +1000

    imported rsa module into encryption service

--- src/app/encryption.service.ts
+++ src/app/encryption.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { NodeRsa } from 'node-rsa';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EncryptionService {
 
-  constructor() { }
+  constructor() {
+    var key = new NodeRsa({b: 512});
+    console.log(key);
+  }
 }
